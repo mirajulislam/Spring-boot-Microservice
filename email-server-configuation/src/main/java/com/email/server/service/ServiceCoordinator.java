5e54aff0ae27360c5d1fac53ce92b7a230263f81
mirajul.islam <mirajul.islam@gtechcp.com>
2022-08-02 16:05:30 +0600

    anatation use

--- email-server-configuation/src/main/java/com/email/server/service/ServiceCoordinator.java
+++ email-server-configuation/src/main/java/com/email/server/service/ServiceCoordinator.java
@@ -10,11 +10,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.email.server.kafka.cons.KafkaConstants;
 import com.email.server.model.SendEmail;
-
+@Service
 public class ServiceCoordinator {
 	
 	private static Logger log = LogManager.getLogger(ServiceCoordinator.class);
