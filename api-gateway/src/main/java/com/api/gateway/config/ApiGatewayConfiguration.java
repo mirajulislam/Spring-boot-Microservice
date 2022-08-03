6c292d4ffb0674204c59a04ae507edf20b837121
mirajul.islam <mirajul.islam@gtechcp.com>
2022-08-03 16:55:13 +0600

    jwt authentication and authorization in spring cloud api gate way

--- api-gateway/src/main/java/com/api/gateway/config/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/api/gateway/config/ApiGatewayConfiguration.java
@@ -1,35 +1,51 @@
 package com.api.gateway.config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.api.gateway.filter.JwtAuthenticationFilter;
+
 @Configuration
 public class ApiGatewayConfiguration {
+	
+	@Autowired
+	private JwtAuthenticationFilter filter;
+
 	@Bean
-	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		//lb means load balancing
-		//uri naming service
-		//path api name
+	public RouteLocator routes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p -> p
-						.path("/get")
-						.filters(f -> f
-								.addRequestHeader("MyHeader", "MyURI")
-								.addRequestParameter("Param", "MyValue"))
-						.uri("http://httpbin.org:80"))
-				.route(p -> p.path("/test-api/**")
-						.uri("lb://email-server"))
-//				.route(p -> p.path("/currency-conversion/**")
-//						.uri("lb://email-server"))
-//				.route(p -> p.path("/currency-conversion-feign/**")
-//						.uri("lb://email-server"))
-//				.route(p -> p.path("/currency-conversion-new/**")
-//						.filters(f -> f.rewritePath(
-//								"/currency-conversion-new/(?<segment>.*)", 
-//								"/currency-conversion-feign/${segment}"))
-//						.uri("lb://currency-conversion"))
-				.build();
+//				.route("auth", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("lb://auth"))
+//				.route("email-server", r -> r.path("/alert/**").filters(f -> f.filter(filter)).uri("lb://alert"))
+//				.route("echo", r -> r.path("/echo/**").filters(f -> f.filter(filter)).uri("lb://echo"))
+				.route("email-server", r -> r.path("/email/**").filters(f -> f.filter(filter)).uri("lb://email-server")).build();
 	}
+	
+//	@Bean
+//	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+//		//lb means load balancing
+//		//uri naming service
+//		//path api name
+//		return builder.routes()
+//				.route(p -> p
+//						.path("/get")
+//						.filters(f -> f
+//								.addRequestHeader("MyHeader", "MyURI")
+//								.addRequestParameter("Param", "MyValue"))
+//						.uri("http://httpbin.org:80"))
+//				.route(p -> p.path("/test-api/**")
+//						.uri("lb://email-server"))
+////				.route(p -> p.path("/currency-conversion/**")
+////						.uri("lb://email-server"))
+////				.route(p -> p.path("/currency-conversion-feign/**")
+////						.uri("lb://email-server"))
+////				.route(p -> p.path("/currency-conversion-new/**")
+////						.filters(f -> f.rewritePath(
+////								"/currency-conversion-new/(?<segment>.*)", 
+////								"/currency-conversion-feign/${segment}"))
+////						.uri("lb://currency-conversion"))
+//				.build();
+//	}
 
 }
--- email-server-configuation/src/main/java/com/email/server/controller/EmailController.java
+++ email-server-configuation/src/main/java/com/email/server/controller/EmailController.java
@@ -6,6 +6,8 @@ import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,30 +18,36 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.email.server.model.SendEmail;
 import com.email.server.service.ServiceCoordinator;
+import com.email.server.util.JwtUtil;
 
 @RestController
 public class EmailController {
 	@Autowired
 	private ServiceCoordinator serviceCoordinator;
 	
-	@RequestMapping(value = "/send-email/attach/", method = RequestMethod.POST)
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@RequestMapping(value = "/email/send-email/attach/", method = RequestMethod.POST)
 	public String sendEmail(@RequestParam("file") MultipartFile file,HttpServletRequest httpServletRequest,@RequestBody SendEmail reqBody) throws MessagingException, IOException {
 		return serviceCoordinator.sendEmailAttachment(reqBody,file);
 	}	
 
-	@RequestMapping(value = "/send-email/", method = RequestMethod.POST)
+	@RequestMapping(value = "/email/send-email/", method = RequestMethod.POST)
 	@ResponseBody
 	public String sendEmail(HttpServletRequest httpServletRequest,@RequestBody SendEmail reqBody)
 			throws MessagingException, IOException {
 		return serviceCoordinator.sendEmail(httpServletRequest, reqBody);
 	}
 	
-	@RequestMapping(value = "/test-api/", method = RequestMethod.GET)
-	public String testApi() {
-		return "Without Jwt";
+	@RequestMapping(value = "/email/test-api/", method = RequestMethod.GET)
+	public ResponseEntity<String> testApi(@RequestBody String userName) {
+		String token = jwtUtil.generateToken(userName);
+
+		return new ResponseEntity<String>(token, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/jwt-api/", method = RequestMethod.GET)
+	@RequestMapping(value = "/email/jwt-api/", method = RequestMethod.GET)
 	public String jwtApi() {
 		return "With Jwt";
 	}
