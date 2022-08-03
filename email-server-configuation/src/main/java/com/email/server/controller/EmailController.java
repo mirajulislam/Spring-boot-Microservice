6eb75ac7ec78565a1057543ec6864560ab46e406
mirajul.islam <mirajul.islam@gtechcp.com>
2022-08-03 17:04:04 +0600

    controller change

--- email-server-configuation/src/main/java/com/email/server/controller/EmailController.java
+++ email-server-configuation/src/main/java/com/email/server/controller/EmailController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,7 +49,7 @@ public class EmailController {
 	}
 	
 	@RequestMapping(value = "/email/jwt-api/", method = RequestMethod.GET)
-	public String jwtApi() {
-		return "With Jwt";
+	public ResponseEntity<String> jwtApi() {
+		return new ResponseEntity<String>("Your alert message, " + "Ok", HttpStatus.OK);
 	}
 }
